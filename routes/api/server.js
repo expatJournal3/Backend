694ecdef7ad7ded67c9698222b770959a6f3c731
aaronjan <aaronjan98@gmail.com>
2020-02-29 18:13:47 -0800

    imported express, helmet & cors inside server.js
    also created index.js and .env file for later use
    
    Co-Authored-By: Devanee Reid <devaneereid@users.noreply.github.com>

---
+++ routes/api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+// middleware
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+// routes
+
+module.exports = server;
