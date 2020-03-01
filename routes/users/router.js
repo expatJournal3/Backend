d1c4d063f7e3ddedb578634b7081e76a53e76e85
aaronjan <aaronjan98@gmail.com>
2020-03-01 00:04:45 -0800

    creat GET /api/users/ endpoint
    Co-Authored-By: Devanee Reid <devaneereid@users.noreply.github.com>

--- routes/users/router.js
+++ routes/users/router.js
@@ -2,16 +2,15 @@ const Users = require('./model.js');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.json({ users: 'up' });
-    // Users.find()
-    //     .then(users => {
-    //         console.log('users', users);
-    //         res.json(users);
-    //     })
-    //     .catch(err => {
-    //         console.log('err', err);
-    //         res.send(err);
-    //     })
+    Users.find()
+        .then(users => {
+            console.log('users', users);
+            res.json(users);
+        })
+        .catch(err => {
+            console.log('err', err);
+            res.send(err);
+        })
 });
 
 router.post('/', (req, res) => {
