07ad8d63f2b3e5bbc907432440586a196199ff53
aaronjan <aaronjan98@gmail.com>
2020-03-03 15:35:05 -0800

    created GET for all users' posts
    Co-Authored-By: Devanee Reid <devaneereid@users.noreply.github.com>

--- routes/paths/router.js
+++ routes/paths/router.js
@@ -1,7 +1,7 @@
 const Paths = require('./model.js');
 const router = require('express').Router();
 
-router.get('/placeholder',(req, res) => {
+router.get('/',(req, res) => {
     Paths.find()
         .then(paths => {
             console.log('paths', paths);
